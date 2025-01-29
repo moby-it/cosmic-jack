8d84876a21f85c6631a0d8b264d94bfa5d1f35a3
George Spanos <gspanos@moby-it.com>
2025-01-30 01:17:34 +0200

    chore: improve texts on tutorial

--- levels/tutorial.gd
+++ levels/tutorial.gd
@@ -24,12 +24,12 @@ var curr_tab_idx = 0
 func _ready() -> void:
 	tabs[curr_tab_idx].visible = true
 
-func _on_skip_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		closed.emit()
-		self.queue_free()
-
 func _on_tab_bar_tab_changed(tab: int) -> void:
 	tabs[curr_tab_idx].visible = false
 	tabs[tab].visible = true
 	curr_tab_idx = tab
+
+
+func _on_close_button_down() -> void:
+	closed.emit()
+	self.queue_free()
