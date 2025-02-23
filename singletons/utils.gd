cecd729befb397f1675ac89531975bd774745521
George Spanos <gspanos@moby-it.com>
2025-02-23 12:16:58 +0200

    remove score

--- singletons/utils.gd
+++ singletons/utils.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 func is_mouse_left(event) -> bool:
 	return event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.is_pressed()
 func is_mouse_right(event) -> bool:
